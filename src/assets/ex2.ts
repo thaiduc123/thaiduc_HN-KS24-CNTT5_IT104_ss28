3d20420f356cc2f0615ffedc5f8f45fcec554984
thaiduc123 <thaihongduc2006@gmail.com>
2025-09-15 13:42:51 +0700

    ss28

--- src/assets/ex2.ts
+++ src/assets/ex2.ts
@@ -1,15 +1,9 @@
-function delayedCallback(callback, delay) {
-  if (typeof callback !== "function") {
-    throw new Error("callback phải là một hàm");
-  }
-  if (typeof delay !== "number" || delay < 0) {
-    throw new Error("delay phải là số nguyên không âm");
-  }
-  setTimeout(callback, delay);
+function delayedCallback(callback: () => void, delay: number): void {
+  setTimeout(() => {
+    callback();
+  }, delay);
 }
 
-function sayHello() {
-  console.log("Xin chào sau 2 giây!");
-}
-
-delayedCallback(sayHello, 2000);
+delayedCallback(() => {
+  console.log("Callback được gọi sau 2 giây!");
+}, 2000);
